f855f7b726569b31cdf9e0f42c27af49256e6f2f
Samuel Maddock <samuel-maddock@hotmail.com>
2019-10-27 23:07:50 +0000

    Added normal distribution simulations for HCMS and CMS

--- simulations/cms_normal_dist.py
+++ simulations/cms_normal_dist.py
@@ -1,50 +1,57 @@
-import xxhash
 import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
-from algorithms.apple_ldp.cms.client.client_cms import client_cms
-from algorithms.apple_ldp.cms.server.sketch_cms import sketch_cms
-from algorithms.apple_ldp.cms.server.server_cms import server_cms
+from algorithms.apple_ldp.cms.client.ClientCMS import ClientCMS
+from algorithms.apple_ldp.cms.server.SketchGenerator import SketchGenerator
+from algorithms.apple_ldp.cms.server.ServerCMS import ServerCMS
+from algorithms.apple_ldp.cms.CMSHelper import cms_helper
 
-def generate_hash_funcs(k, m):
-    hash_funcs = []
-    for i in range (0,k):
-        hash_funcs.append(lambda data: xxhash.xxh64(data, seed=k).intdigest() % m)
-    return hash_funcs
 
-m = 5000
+# -------------------- Parameters for simulation --------------------
+m = 4096
 k = 1024
 epsilon = 4
-N = 5000
+
+N = 10000
 mu = 10
 sd = 3
 
-hash_funcs = generate_hash_funcs(k, m)
+# -------------------- Simulating the client-side process --------------------
 
-ldp_data = []
+hash_funcs = cms_helper.generate_hash_funcs(k, m)
+
 data = np.random.normal(mu,sd,N).astype(int)
+ldp_data = []
+
 bins = np.arange(start=min(data), stop = max(data) + 1)
 fig, axs = plt.subplots(3)
 
+client_cms = ClientCMS(epsilon, hash_funcs, m)
+sketch_generator = SketchGenerator(epsilon, k, m)
+
 print("Sampling data from the clients...")
 for i in range(0,N):
-    ldp_data.append(client_cms(epsilon, data[i], hash_funcs, m))
+    ldp_data.append(client_cms.client_cms(data[i]))
 
-M = sketch_cms(ldp_data, epsilon, k, m)
+# -------------------- Simulating the server-side process --------------------
 
-print(server_cms(np.int64(10), M, hash_funcs))
+M = sketch_generator.create_cms_sketch(ldp_data)
+
+server_cms = ServerCMS(M, hash_funcs)
 
 print("Estimating frequencies from sketch matrix...")
 ldp_freq = np.empty(len(bins))
 ldp_plot_data = np.empty(len(bins))
 for key in bins:
-    ldp_freq[key-1] = server_cms(key, M, hash_funcs)
+    ldp_freq[key-1] = server_cms.estimate_freq(key)
     ldp_plot_data = np.append(ldp_plot_data, [key]*int(ldp_freq[key-1]))
+
+# -------------------- Plotting the data --------------------
 print("Plotting data...")
 
 # Plotting a distplot of our integer data sampled from a normal dist
 sns.distplot(data, bins=bins, ax=axs[0], hist_kws={'ec': "black"})
-axs[0].set_title("Integer data sampled from a Normal distribution \n $N=${}, Sampled from $N({}, {})$".format(N, mu, sd*sd))
+axs[0].set_title("Count Mean Sketch (CMS) \n Integer data sampled from a Normal distribution \n $N=${}, Sampled from $N({}, {})$".format(N, mu, sd*sd))
 # Plotting a distplot of the data produced from the CMS algorithm
 sns.distplot(ldp_plot_data, bins=bins, ax=axs[1], color='r', hist_kws={'ec': "black"})
 axs[1].set_title("LDP CMS data produced from the normal sample \n $\epsilon=${}, $m=${}, $k=${}".format(epsilon, m, k))
@@ -54,4 +61,3 @@ sns.distplot(data, bins=bins, hist=False, ax=axs[2])
 plt.show()
 
 print("Plot Displayed !")
-print(ldp_freq)
---
+++ simulations/hcms_normal_dist.py
@@ -0,0 +1,63 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+from algorithms.apple_ldp.cms.client.ClientCMS import ClientCMS
+from algorithms.apple_ldp.cms.server.SketchGenerator import SketchGenerator
+from algorithms.apple_ldp.cms.server.ServerCMS import ServerCMS
+from algorithms.apple_ldp.cms.CMSHelper import cms_helper
+
+# -------------------- Parameters for simulation --------------------
+m = 4096
+k = 1024
+epsilon = 4
+
+N = 10000
+mu = 10
+sd = 3
+
+# -------------------- Simulating the client-side process --------------------
+
+hash_funcs = cms_helper.generate_hash_funcs(k, m)
+
+data = np.random.normal(mu,sd,N).astype(int)
+ldp_data = []
+
+bins = np.arange(start=min(data), stop = max(data) + 1)
+fig, axs = plt.subplots(3)
+
+client_cms = ClientCMS(epsilon, hash_funcs, m)
+sketch_generator = SketchGenerator(epsilon, k, m)
+
+print("Sampling data from the clients...")
+for i in range(0,N):
+    ldp_data.append(client_cms.client_hcms(data[i]))
+
+# -------------------- Simulating the server-side process --------------------
+
+M = sketch_generator.create_hcms_sketch(ldp_data) # Constructing the sketch matrix
+
+server_cms = ServerCMS(M, hash_funcs)
+
+print("Estimating frequencies from sketch matrix...")
+ldp_freq = np.empty(len(bins)) # Stores estimated frequencies for each data point (
+ldp_plot_data = np.empty(len(bins)) # Stores the actual data
+for key in bins:
+    ldp_freq[key-1] = server_cms.estimate_freq(key)
+    ldp_plot_data = np.append(ldp_plot_data, [key]*int(ldp_freq[key-1]))
+
+# -------------------- Plotting the data --------------------
+
+print("Plotting data...")
+
+# Plotting a distplot of our integer data sampled from a normal dist
+sns.distplot(data, bins=bins, color='g', ax=axs[0], hist_kws={'ec': "black"})
+axs[0].set_title("Hadamard CMS \n Integer data sampled from a Normal distribution \n $N=${}, Sampled from $N({}, {})$".format(N, mu, sd*sd))
+# Plotting a distplot of the data produced from the CMS algorithm
+sns.distplot(ldp_plot_data, bins=bins, ax=axs[1], color='y', hist_kws={'ec': "black"})
+axs[1].set_title("LDP HCMS data produced from the normal sample \n $\epsilon=${}, $m=${}, $k=${}".format(epsilon, m, k))
+# Plotting both kde's from above for comparison
+sns.distplot(ldp_plot_data, hist=False, bins=bins, ax=axs[2], color='y')
+sns.distplot(data, bins=bins, hist=False, ax=axs[2], color='g')
+plt.show()
+
+print("Plot Displayed !")
