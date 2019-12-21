7f3b075ba4c33f02278fa05351fff858a3f0aaf8
Samuel Maddock <samuel-maddock@hotmail.com>
2019-12-21 19:52:01 +0000

    Refactored cms_helper

--- algorithms/apple_ldp/cms/CMSHelper.py
+++ algorithms/apple_ldp/cms/CMSHelper.py
@@ -5,11 +5,13 @@ class CMSHelper:
     def generate_hash_funcs(self, k, m):
         hash_funcs = []
         for i in range(0, k):
-            hash_funcs.append(lambda data: xxhash.xxh64(data, seed=i).intdigest() % m)
+            hash_funcs.append(self.generate_hash(m, i))
         return hash_funcs
 
     def generate_256_hash(self):
         return lambda data: xxhash.xxh64(data, seed=10).intdigest() % 256
 
+    def generate_hash(self, m, seed):
+        return lambda data: xxhash.xxh64(str(data), seed=seed).intdigest() % m
 
 cms_helper = CMSHelper()
