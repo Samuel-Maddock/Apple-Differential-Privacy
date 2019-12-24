97aa6e3cf2006fbbc6511ea3996d0da44f470368
Samuel Maddock <samuel-maddock@hotmail.com>
2019-12-24 14:35:35 +0000

    RAPPOR client generate unique secrets

--- algorithms/google_ldp/rappor/client/RAPPORClient.py
+++ algorithms/google_ldp/rappor/client/RAPPORClient.py
@@ -1,11 +1,11 @@
 from algorithms.google_ldp.rappor.client.rappor import Encoder, SecureIrrRand
-
+import uuid
 
 class RAPPORClient:
     def __init__(self, cohort, params):
         self.cohort = cohort
         self.params = params
-        self.encoder = Encoder(self.params, self.cohort, "secret", SecureIrrRand(self.params))
+        self.encoder = Encoder(self.params, self.cohort, uuid.uuid4().bytes, SecureIrrRand(self.params))
 
     def generate_report(self, data):
         return (self.encoder.encode(data), self.cohort)
