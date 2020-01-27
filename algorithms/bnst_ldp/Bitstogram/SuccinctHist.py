aa5108c931543b0fb7225f3cdd9f9751a27583fa
Samuel Maddock <samuel-maddock@hotmail.com>
2020-01-27 14:52:59 +0000

    Fixed SuccinctHist to work for general strings

--- algorithms/bnst_ldp/Bitstogram/SuccinctHist.py
+++ algorithms/bnst_ldp/Bitstogram/SuccinctHist.py
@@ -1,28 +1,28 @@
 import numpy as np
 import math
-import itertools
-import random
 from algorithms.apple_ldp.cms.CMSHelper import cms_helper
+from algorithms.bnst_ldp.Bitstogram.Hashtogram import Hashtogram
 from algorithms.bnst_ldp.Bitstogram.ExplicitHist import ExplicitHist
-from collections import Counter
+from bitstring import BitArray
 
 # Any set [n] = {1,...,n} is represented here as {0,....,n-1} for ease of indexing
 
-# Dataset passed to it should be binary strings of length no more than logd
+# d should be the binary length of the strings in the dataset
+    # Data should be padded to some max length prior to using SuccinctHist
+    # TODO: String padding...
 
 class SuccintHist:
-    def __init__(self, dataset, hash, T, d, epsilon):
+    def __init__(self, dataset, T, d, epsilon):
         self.epsilon = epsilon
         self.prob = 1 / ((math.e ** epsilon) + 1)
         self.n = len(dataset)
-        self.d = d
         self.T = T
-        self.hash = hash
-        self.binary_domain_size = math.ceil(math.log(d, 2))
+        self.hash = cms_helper.generate_hash(T, 2)
+        self.binary_domain_size = d
         self.partition = self.__generate_partition(self.n)
         self.dataset = dataset
 
-        dataset = [str(i) for i in dataset]
+        #dataset = [BitArray(bytes=bytes(x, "utf-8")).bin for x in dataset]
 
         # Constructing randomised dataset
         S = {}
@@ -30,13 +30,10 @@ class SuccintHist:
             tuple_set = []
             for v in self.partition[l]:
                 data = dataset[v]
-                tuple_set.append((self.hash(data), data[l])) # Hash data item and sample lth bit
+                tuple_set.append((self.hash(data), BitArray(bytes=bytes(data, "utf-8")).bin[l])) # Hash data item and sample lth bit
             S[l] = tuple_set
 
         self.S = S
-
-    def __basic_randomiser(self, x):
-        return np.random.choice([x, -x], 1, p=[1 - self.prob, self.prob])
 
     # Public Randomness Partition - Rework this into a general client-server model
     def __generate_partition(self, n):
@@ -58,9 +55,9 @@ class SuccintHist:
         hist_list = []
         for l in range(0, self.binary_domain_size):
             s = self.S[l]
-            hist_list.append(ExplicitHist(s, self.T * 2, self.epsilon/2, index_map=self.__index_mapper))
+            hist_list.append(ExplicitHist(s, self.T * 2, self.epsilon, index_map=self.__index_mapper))
 
-        S = []
+        S = set()
 
         for t in range(0, self.T):
             # Build up heavy hitters bit by bit
@@ -72,7 +69,11 @@ class SuccintHist:
                     v = v + "0"
                 else:
                     v = v + "1"
-            S.append(v)
+
+            try:
+                S.add(BitArray(bin=v).tobytes().decode("utf-8"))
+            except:
+                pass # Sometimes random noise will generate strings that can't be decoded to utf-8
 
 
         # Below is computationally poor, since we construct a histogram with a matrix of n x n
@@ -81,23 +82,12 @@ class SuccintHist:
         # The full Bitstogram algorithm deals with this problem
         hist = ExplicitHist(self.dataset, len(self.dataset), self.epsilon/2, index_map=cms_helper.generate_hash(len(self.dataset), seed=3))
 
-        return list(map(lambda x: (x, hist.freq_oracle(x)), S)) # Output heavy hitters + freq estimation
+        heavy_hitters = []
+        print(S)
+        for heavy_hitter in S:
+            freq = hist.freq_oracle(heavy_hitter)
+            if freq > math.sqrt(len(self.dataset)): # Heuristic to prune false positives
+                heavy_hitters.append((heavy_hitter, freq))
 
-
-
-# Synthetic data
-data1 = ["1011"] * 10000
-data2 = ["1111"] * 8000
-data3 = ["1000"] * 300
-
-data = np.concatenate((data1,data2,data3))
-
-# We use T = 2, d = 10, epsilon = 1
-    # Our threshold is 2, since our original dataset only contains 3 unique elements
-
-h = cms_helper.generate_hash(2, 2)
-heavy_hitters = SuccintHist(data, h, 2, 10, 1)
-
-heavy_hitter_list = heavy_hitters.find_heavy_hitters()
-
-print(heavy_hitter_list)
+        print(heavy_hitters)
+        return heavy_hitters # Output heavy hitters + freq estimation
