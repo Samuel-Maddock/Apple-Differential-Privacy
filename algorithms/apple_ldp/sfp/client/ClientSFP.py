7c232762352bf662fff41485ca0fc8cfbf59f206
Samuel Maddock <samuel-maddock@hotmail.com>
2020-01-29 13:02:35 +0000

    Generalised SFP Support

--- algorithms/apple_ldp/sfp/client/ClientSFP.py
+++ algorithms/apple_ldp/sfp/client/ClientSFP.py
@@ -3,15 +3,25 @@ from algorithms.apple_ldp.cms.client.ClientCMS import ClientCMS
 
 
 class ClientSFP:
-    def __init__(self, cms_params, hash_families, hash_256):
+    def __init__(self, cms_params, hash_families, hash_256, fragment_length=2, max_string_length=6, padding_char="*"):
         epsilon, m = cms_params[0]
         epsilon_prime, m_prime = cms_params[1]
         self.hash_256 = hash_256
         self.word_cms = ClientCMS(epsilon, hash_families[0], m)
         self.fragment_cms = ClientCMS(epsilon_prime, hash_families[1], m_prime)
+        self.max_string_length = max_string_length
+        self.padding_char = padding_char
+        self.fragment_length = fragment_length
 
     def fragment(self, string):
-        odd_numbers = np.arange(1, len(string), step=2)
-        l = np.random.choice(odd_numbers)
-        r = str(self.hash_256(string)) + string[l-1: l + 1]
+
+        # Pad strings that are smaller than some arbitrary max value
+        if len(string) < self.max_string_length:
+            string += (self.max_string_length - len(string)) * self.padding_char
+        elif len(string) > self.max_string_length:
+            string = string[0:self.max_string_length]
+
+        fragment_indices = np.arange(1, len(string), step=self.fragment_length)
+        l = np.random.choice(fragment_indices)
+        r = str(self.hash_256(string)) + "_" + string[l-1: l + (self.fragment_length-1)]
         return self.fragment_cms.client_cms(r), self.word_cms.client_cms(string), l
--- algorithms/apple_ldp/sfp/server/ServerSFP.py
+++ algorithms/apple_ldp/sfp/server/ServerSFP.py
@@ -10,12 +10,15 @@ import itertools
 
 
 class ServerSFP:
-    def __init__(self, cms_params, hash_families, threshold):
+    def __init__(self, cms_params, hash_families, threshold, fragment_length=2, max_string_length=6, padding_char="*"):
         Parameters = namedtuple("Parameters", ["epsilon", "k", "m"])
         self.word_parameters = Parameters(*cms_params[0])
         self.fragment_parameters = Parameters(*cms_params[1])
         self.word_hash_functions, self.fragment_hash_functions = hash_families
         self.threshold = threshold
+        self.fragment_length = fragment_length
+        self.max_string_length = max_string_length
+        self.padding_char = padding_char
 
     def __create_fragment_estimators(self, data, indexes):
         estimator_dict = {}
@@ -35,12 +38,11 @@ class ServerSFP:
             frequencies = []
             for fragment in fragments:
                 frequency = estimator[1](fragment)
-                print(frequency)
                 frequencies.append(frequency)
 
     def __split_fragment(self, fragment):
-        fragment_split = re.split(r'(\d+)', fragment)
-        return fragment_split[1], fragment_split[2]
+        fragment_split = fragment.split("_", 1)
+        return fragment_split[0], fragment_split[1]
 
     def generate_frequencies(self, sfp_data, alphabet):
         alpha_list, beta_list, index_list = list(zip(*sfp_data))
@@ -55,27 +57,27 @@ class ServerSFP:
         fragments = []
         frequency_dict = defaultdict(lambda: HeavyHitterList(self.threshold))
 
-        fragments = list(map(lambda x: x[0]+x[1], itertools.product(alphabet, alphabet)))
+        fragments = list(map(lambda x: x[0] + x[1], itertools.product(alphabet, alphabet)))
 
         for i in range(0, 256):
             for fragment in fragments:
-                fragment = str(i) + str(fragment)
+                fragment = str(i) + "_" + str(fragment)
                 for estimator in estimator_list.items():
                     fragment_freq_pair = fragment, estimator[1](fragment)
                     frequency_dict[estimator[0]].append(fragment_freq_pair)
 
         hash_table = defaultdict(lambda: defaultdict(list))
 
-        odd_numbers = np.arange(1, 6, step=2) # TODO: Hacky
-        for l in odd_numbers:
+        fragment_indices = np.arange(1, self.max_string_length, step=self.fragment_length)
+        for l in fragment_indices:
             fragments, frequencies = zip(*frequency_dict.get(l).get_data())
             for fragment in fragments:
-                key,value = self.__split_fragment(fragment)
+                key, value = self.__split_fragment(fragment)
                 hash_table[key][l].append(value)
 
         for dictionary in hash_table.values():
             fragment_list = list(dictionary.values())
-            if len(fragment_list) == 3: # TODO: Hacky
-                D += list(map(lambda x: str().join(x), itertools.product(*fragment_list)))
+            if len(fragment_list) == int(self.max_string_length / self.fragment_length):
+                D += list(map(lambda x: str().join(x).strip(self.padding_char), itertools.product(*fragment_list)))
 
         return D, freq_oracle
