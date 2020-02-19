39533f9c4fc49dc29aae91b9bc5283d29fa8ec4a
Samuel Maddock <samuel-maddock@hotmail.com>
2020-02-19 17:15:43 +0000

    Sketch Matrix Performance Fix
    No longer generates sketch vectors multiple times

--- algorithms/apple_ldp/cms/server/SketchGenerator.py
+++ algorithms/apple_ldp/cms/server/SketchGenerator.py
@@ -11,26 +11,15 @@ class SketchGenerator:
         self.c = (math.pow(math.e, epsilon / 2) + 1) / (math.pow(math.e, epsilon / 2) - 1)
 
     def create_cms_sketch(self, dataset):
-        x = np.zeros((self.m, len(dataset)))
-        print("Generating sketch vectors...")
-        for i in range(0, len(dataset)):
-            v = np.array(dataset[i][0])  # Retrieve privatised vector
-            ones_vector = np.array([1] * self.m)
-            entry = self.k * ((self.c / 2) * v + (1 / 2) * ones_vector)
-            x[:, i] = entry
-
         M = np.zeros((self.k, self.m))
-        print("Constructing sketch matrix...")
-        for i in range(0, len(dataset)):
-            for j in range(0, self.m):
-                hash_index = dataset[i][1]
-                M[hash_index][j] = M[hash_index][j] + x[:, i][j]
-        print("Sketch Matrix Created...")
+        ones_vector = np.ones(self.m)
+
+        for data in dataset:
+            hash_index = data[1]
+            M[hash_index] = M[hash_index] + self.k * ((self.c / 2) * data[0] + 0.5 * ones_vector)
         return M
 
     def create_hcms_sketch(self, dataset):
-        print("Generating Hadamard Sketch Matrix...")
-        x = np.zeros(len(dataset))
         M = np.zeros((self.k, self.m))
 
         for i in range(0, len(dataset)):
@@ -38,8 +27,6 @@ class SketchGenerator:
             j = dataset[i][1]
             l = dataset[i][2]
 
-            x[i] = self.k * self.c * bit_value
-            M[j][l] = M[j][l] + x[i]
+            M[j][l] = M[j][l] + self.k * self.c * bit_value
 
-        print("Sketch Matrix Created")
         return np.matmul(M, np.transpose(hadamard(self.m)))
