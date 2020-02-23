b01943ea854641c2dd9f9d1b7b379331d3c828b6
Samuel Maddock <samuel-maddock@hotmail.com>
2020-02-23 18:37:08 +0000

    TreeHistogram support for any freq oracle

--- algorithms/bnst_ldp/TreeHistogram/ServerSide.py
+++ algorithms/bnst_ldp/TreeHistogram/ServerSide.py
@@ -6,8 +6,10 @@ import uuid
 import itertools
 
 from algorithms.bnst_ldp.TreeHistogram.PrivateCountSketch import PrivateCountSketch
-from collections import deque
-
+from algorithms.bnst_ldp.Bitstogram.Hashtogram import Hashtogram
+from algorithms.bnst_ldp.Bitstogram.ExplicitHist import ExplicitHist
+from algorithms.apple_ldp.cms.server.ServerCMS import ServerCMS
+from collections import deque, Counter
 
 class TreeHistogram:
     def __init__(self, l, w, epsilon, num_n_grams, gram_length, threshold, alphabet):
@@ -19,6 +21,7 @@ class TreeHistogram:
         self.threshold = threshold
         self.empty_char = "*"
 
+        # TODO: Test this
         if "*" in alphabet:
             alphabet.remove(self.empty_char)
 
@@ -40,8 +43,7 @@ class TreeHistogram:
     # configFileName: File to dump the configuration parameters. Include expt info in the filename
     # expResultFile: File to dump the experiment results as a .csv file
 
-    def runServerSide(self, word_frequency):
-
+    def run_server_side(self, word_frequency):
         priv_count_sketch = PrivateCountSketch(self.l, self.w, self.epsilon)
 
         for index, row in word_frequency.iterrows():
@@ -60,42 +62,59 @@ class TreeHistogram:
         print(word_frequency)
         return word_frequency
 
-    def runServerSideWordDiscovery(self, word_frequency):
-
+    def run_server_side_word_discovery(self, data, freq_oracle=None, freq_oracle_params=None):
         word_length = self.num_n_grams * self.gram_length
-
+        word_frequency = pd.DataFrame(list(dict(Counter(data)).items()), columns=["word", "trueFrequency"])
         pres_rec_df = pd.DataFrame(['NumOfWords', 'Precision', 'Recall'], columns=['Measure'])
 
-        priv_count_sketch = PrivateCountSketch(self.l, self.w, self.epsilon, use_median=True)
-        main_count_sketch = PrivateCountSketch(self.l, self.w, self.epsilon)
-
         # Simulating client-side
-        for index, row in word_frequency.iterrows():
-            for i in range(row['trueFrequency']):
-                current_word = row['word']
-                main_count_sketch.set_sketch_element(current_word)
+            # PrivateCountSketch is the standard freq oracle for TreeHistogram
+            # Can also provide other frequency oracles for simulation results
 
-                if len(current_word) <= word_length:
-                    current_word += self.empty_char * (word_length - len(current_word))
-                else:
-                    current_word = current_word[:word_length]
+        if freq_oracle is None or freq_oracle_params is None:
+            fragment_estimator = PrivateCountSketch(self.l, self.w, self.epsilon, use_median=True)
+            word_estimator = PrivateCountSketch(self.l, self.w, self.epsilon)
 
-                word_to_send = self.__choose_random_n_gram_prefix(current_word, self.gram_length)
+            for index, row in word_frequency.iterrows():
+                for i in range(row['trueFrequency']):
+                    current_word = row['word']
+                    word_estimator.set_sketch_element(current_word)
 
-                priv_count_sketch.set_sketch_element(word_to_send)
+                    if len(current_word) <= word_length:
+                        current_word += self.empty_char * (word_length - len(current_word))
+                    else:
+                        current_word = current_word[:word_length]
 
-        # Server-side Frequency estimation section
+                    word_to_send = self.__choose_random_n_gram_prefix(current_word, self.gram_length)
+
+                    fragment_estimator.set_sketch_element(word_to_send)
+        else:
+            params = freq_oracle_params
+            freq_oracles = {
+                "priv_count_sketch": lambda dataset: PrivateCountSketch(**params, data=dataset),
+                "priv_count_sketch_median": lambda dataset: PrivateCountSketch(**params, data=dataset, use_median=True),
+                "explicithist": lambda dataset: ExplicitHist(dataset, **params),
+                "hashtogram": lambda dataset: Hashtogram(dataset, **params),
+                "hashtogram_median": lambda dataset: Hashtogram(dataset, **params, use_median=True),
+                "cms": lambda dataset: ServerCMS(dataset, **params, is_raw_data=True),
+                "hcms": lambda dataset: ServerCMS(dataset, **params, is_hadamard=True, is_raw_data=True)
+            }
+
+            word_estimator = freq_oracles.get(freq_oracle)(data)
+            fragments = list(map(lambda word: self.__choose_random_n_gram_prefix(word, self.gram_length), data))
+            fragment_estimator = freq_oracles.get(freq_oracle)(fragments)
+
+        # ------------------ Server-side Frequency estimation section ------------------
         scaling_factor = self.num_n_grams
         n_gram_set = self.__gen_english_n_grams(self.alphabet)
         list_n_grams = [s + self.empty_char * (word_length - len(s)) for s in n_gram_set]
-        print(n_gram_set)
         word_queue = deque(list_n_grams)
         noisy_frequencies = {}
 
         while word_queue.__len__() != 0:
             current_prefix = word_queue.popleft()
             current_prefix_after_stripping_empty = current_prefix.replace(self.empty_char, '')
-            freq_for_current_prefix = int(priv_count_sketch.freq_oracle(current_prefix))
+            freq_for_current_prefix = int(fragment_estimator.freq_oracle(current_prefix))
 
             if freq_for_current_prefix < self.threshold:
                 continue
@@ -135,7 +154,7 @@ class TreeHistogram:
         # Reapproximate frequencies based on the minimum of the estimates of fragment frequency vs whole dataset frequenc
         heavy_hitters = {}
         for key, value in noisy_frequencies.items():
-            freq = main_count_sketch.freq_oracle(key)
+            freq = word_estimator.freq_oracle(key)
             if min(value,freq) >= 0:
                 heavy_hitters[key] = max(value,freq)
 
