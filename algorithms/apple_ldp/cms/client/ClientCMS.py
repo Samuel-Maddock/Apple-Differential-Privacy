f9f2882347b2c71dc971456976f77e537a2388e2
Samuel Maddock <samuel-maddock@hotmail.com>
2020-05-01 21:09:06 +0100

    Fast Hadamard Trick
    Using sparsity of one-hot vector

--- algorithms/apple_ldp/cms/client/ClientCMS.py
+++ algorithms/apple_ldp/cms/client/ClientCMS.py
@@ -3,47 +3,36 @@ from scipy.linalg import hadamard
 import math
 import time
 
+
 class ClientCMS:
     def __init__(self, epsilon, hash_funcs, m):
         self.epsilon = epsilon
         self.hash_funcs = hash_funcs
         self.k = len(hash_funcs)
         self.m = m
-        self.prob = 1/(1+math.pow(math.e, self.epsilon/2))
+        self.prob = 1 / (1 + math.pow(math.e, self.epsilon / 2))
+        self.had = hadamard(self.m)
 
     def __privatise(self, data, is_hadamard=False):
         j = np.random.randint(0, self.k)
         h_j = self.hash_funcs[j]
-        v = [0]*self.m if is_hadamard else [-1]*self.m
+        v = [0] * self.m if is_hadamard else [-1] * self.m
         v[h_j(data)] = 1
-        b = np.random.choice([-1, 1], 1 if is_hadamard else self.m, p=[self.prob, 1-self.prob])
-        return v,b,j
+        b = np.random.choice([-1, 1], 1 if is_hadamard else self.m, p=[self.prob, 1 - self.prob])
+        return v, b, j
 
     def client_cms(self, data):
-        v,b,j = self.__privatise(data)
+        v, b, j = self.__privatise(data)
         return np.multiply(v, b), j
-
-    # Fast-Walsh Hadamard Transform for O(nlogn) performance
-        # https://en.wikipedia.org/wiki/Fast_Walsh–Hadamard_transform
-    def fwht(self, a):
-        """In-place Fast Walsh–Hadamard Transform of array a."""
-        h = 1
-        while h < len(a):
-            for i in range(0, len(a), h * 2):
-                for j in range(i, i + h):
-                    x = a[j]
-                    y = a[j + h]
-                    a[j] = x + y
-                    a[j + h] = x - y
-            h *= 2
-        return a
 
     def client_hcms(self, data):
         if not (self.m & (self.m - 1)) == 0:
             raise ValueError("m must be a positive integer, and m must be a power of 2 to use hcms")
 
-        v,b,j = self.__privatise(data, is_hadamard=True)
-        w = self.fwht(v) # Hadamard transform H_m.v
+        v, b, j = self.__privatise(data, is_hadamard=True)
+        # w = self.fwht(v) # Hadamard transform H_m.v
+        h_j = self.hash_funcs[j]
+        w = self.had[:, h_j(data)]
 
         l = np.random.randint(0, self.m)
-        return b[0]*w[l], j, l # Return (b*w_l, index j, index l)
+        return b[0] * w[l], j, l  # Return (b*w_l, index j, index l)
